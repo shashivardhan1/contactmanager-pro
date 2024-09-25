b0a959570b716d4ccfedde85d217695bb8987924
shashivardhan1 <d.shashivardhanreddy999@gmail.com>
2024-09-25 23:14:58 +0530

    added the morgan

--- server/app.js
+++ server/app.js
@@ -1,18 +1,20 @@
-const express = require('express');
-require('express-async-errors');
-const cors = require('cors');
-const middleware = require('./utils/middleware');
-const authRoutes = require('./routes/auth');
-const contactRoutes = require('./routes/contact');
+const express = require("express");
+require("express-async-errors");
+const cors = require("cors");
+const middleware = require("./utils/middleware");
+const authRoutes = require("./routes/auth");
+const contactRoutes = require("./routes/contact");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(morgan("dev"));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.use('/api', authRoutes);
-app.use('/api/contacts', contactRoutes);
+app.use("/api", authRoutes);
+app.use("/api/contacts", contactRoutes);
 
 app.use(middleware.unknownEndpointHandler);
 app.use(middleware.errorHandler);
